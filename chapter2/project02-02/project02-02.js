e0763450fe3265b15157925a08344b9263d3b3a1
GrahamHatten <123115852+GrahamHatten@users.noreply.github.com>
2023-01-31 08:48:27 -0600

    Update project02-02.js

--- chapter2/project02-02/project02-02.js
+++ chapter2/project02-02/project02-02.js
@@ -12,12 +12,17 @@
             let name = document.getElementById("name").value;
             let email = document.getElementById("email").value;
             let phone = document.getElementById("phone").value;
-
+            const checkForAt = email.indexOf("@");
+                
             (name && email && phone)
                         ? window.alert("Thank you!")
                         : window.alert("Please fill in all fields");
-                
 
+            if(checkForAt !== -1){
+                window.alert("Thank you for the email address");
+            } else{
+                window.alert("Please enter a valid email address");
+            }
             
       }
-            document.getElementById("submit").addEventListener("click",verifyForm) ;
+            document.getElementById("submit").addEventListener("click",verifyForm);
